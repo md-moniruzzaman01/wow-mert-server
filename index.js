2927c3fcb0e69caadf230b73719f12eddc23cc96
md-moniruzzaman1 <md.moniruzzaman1050@gmail.com>
2022-07-20 16:35:22 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const app = express()
+cors = require('cors')
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const port = process.env.PORT || 3000
+
+
+// middelware
+app.use(cors());
+app.use(express.json())
+require('dotenv').config();
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_USER_PASS}@cluster0.azomj.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+      await client.connect();
+      const Collection = client.db("wowmart").collection("products");
+  
+      app.get('/product/:id', async(req,res)=>{
+        const id = req.params.id
+       
+        const query = {_id:ObjectId(id)}
+        const cursor = await Collection.findOne(query)
+        res.send(cursor)
+      })
+    }finally{
+  
+    }
+    
+  }
+  run().catch(console.dir);
+  
+  
+  app.get('/', (req, res) => {
+    res.send('wow server')
+  })
+
+  app.listen(port, () => {
+    console.log(` listening on port ${port}`)
+  })
